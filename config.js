831785dc69341b203a54461550d21ce989a68b5d
PatReeekt05 <patrickmariano.it@gmail.com>
2025-02-09 22:39:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -57,11 +57,11 @@ const CONFIG = {
     // Color scheme for the website
     // Use https://colorhunt.co or https://coolors.co to find beautiful color combinations
     colors: {
-        backgroundStart: "#8D7B68",      // Gradient start (try pastel colors for a soft look)
-        backgroundEnd: "#A4907C",        // Gradient end (should complement backgroundStart)
-        buttonBackground: "#C8B6A6",     // Button color (should stand out against the background)
-        buttonHover: "#C8B6A6",          // Button hover color (slightly lighter than buttonBackground)
-        textColor: "#F1DEC9"             // Text color (make sure it's readable!)
+        backgroundStart: "#B1B2FF",      // Gradient start (try pastel colors for a soft look)
+        backgroundEnd: "#AAC4FF",        // Gradient end (should complement backgroundStart)
+        buttonBackground: "#D2DAFF",     // Button color (should stand out against the background)
+        buttonHover: "#AAC4FF",          // Button hover color (slightly lighter than buttonBackground)
+        textColor: "#EEF1FF"             // Text color (make sure it's readable!)
     },
 
     // Animation settings
